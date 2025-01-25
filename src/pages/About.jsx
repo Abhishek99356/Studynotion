dfab033d252a0d42c246e4373b9dc2fdf357e2f2
Abhishek99356 <abhishekyadav40854@gmail.comgit config --global user.email>
2025-01-26 02:20:18 +0530

    studynotion web

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HighlightText from "../components/core/HomePage/HighlightText"
+
+function About() {
+  return (
+    <div className='mt-[100px] text-white'>
+      {/* section 1 */}
+      <section>
+        <div>
+            <header>
+                Driving Innovation in online Education for a
+                <HighlightText text={"Brighter Future"}/>
+            </header>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default About
